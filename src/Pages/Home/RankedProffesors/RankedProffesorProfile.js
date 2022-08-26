90f0a800e16849c5c9e4ac5b55279cfebde28c9a
FahadwebD <fahadchowdhury66779@gmail.com>
2022-08-26 22:54:16 +0600

    cleaning

--- src/Pages/Home/RankedProffesors/RankedProffesorProfile.js
+++ src/Pages/Home/RankedProffesors/RankedProffesorProfile.js
@@ -1,8 +1,10 @@
+import { Co2Sharp } from '@mui/icons-material';
 import React from 'react';
 
 import { Link } from 'react-router-dom';
 import './RankedProfessor.css'
 const RankedProffesorProfile = ({staff}) => {
+    console.log(staff)
     return (
         <div>
 
@@ -10,7 +12,7 @@ const RankedProffesorProfile = ({staff}) => {
     <div className="rankCard"><img src={`data:image/png;base64,${staff.image}`}  alt="prof"/>
     <h2> {staff.name}</h2>
     <span>{staff.designation}</span>
-    {staff.speech===undefined ?'Nothing':<p> {staff.speech.slice(0, 60)+'...'}</p>}
+    {staff.speech =='undefined'?<p></p>:<p> {staff.speech.slice(0, 60)+'...'}</p>}
     
 
     <Link style={{textDecoration:'none' }} to={`/speech/${staff._id}`}><button>Read More</button></Link> 
